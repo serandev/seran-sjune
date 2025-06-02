bc9345bc4b44c152b420a396bea90c49d3c62f39
seren.a <seren.a@kakaocorp.com>
2025-06-02 20:22:48 +0900

    화면 조정

--- src/components/CoupleSection.tsx
+++ src/components/CoupleSection.tsx
@@ -36,11 +36,11 @@ const CoupleSection: React.FC = () => {
             backgroundPosition: 'center 20%',
           }}
       >
-        <div className="absolute inset-0 flex items-start justify-center pt-8">
-          <div className="container mx-auto px-4 py-2 bg-background/70 backdrop-blur-sm">
+        <div className="absolute inset-0 flex items-start justify-center pt-0 sm:pt-10 md:pt-20">
+          <div className="container mx-auto px-auto py-2 sm:py-10 md:py-10 bg-background/70 backdrop-blur-sm">
             <div className="max-w-3xl mx-auto text-center">
               <h1
-                  className="font-himelody text-5xl pt-20 md:text-7xl text-gray-200 mb-8"
+                  className="font-himelody text-5xl pt-20 sm:text-6xl md:text-7xl text-gray-200 mb-4 md:mb-8"
                   style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.9)' }}
               >
                 저희 결혼합니다
